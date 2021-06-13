1e5cdf46f8b4c4ba2d531f17c95bc78521252cef
tdw535 <tkedwards0@gmail.com>
2021-06-13 16:24:46 -0600

    Gettting build process better

--- generateBuild.sh
+++ generateBuild.sh
@@ -1,8 +1,20 @@
 source ~/.bash_profile 
-cd build
+
 emcmake cmake .
 make
-mkdir bin
-mv ../TwoLayerFlowHead-exe/TwoLayerFlowHead-exe.* bin/
 
+#clean up cmake files
+
+cd build
+rm -rf bin
+mkdir bin
+echo "Done cleaning up bin in build"
+echo "Moving .js .wasm files from TwoLayerFlowHead"
+mv ../cppModules/TwoLayerFlowHead-exe/bin/TwoLayerFlowHead-exe.* bin
+
+echo "Current Dir"
+pwd
+echo "Copying html sample view to bin"
+cp ../webModules/sampleView/helloWorld.html bin
+cd ..
 
